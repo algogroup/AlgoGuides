5618a451640820f8e1b631f9c0b7ef5dbdb2df73
Daniel L <daniellanoff@gmail.com>
2019-05-30 16:02:53 -0400

    updated description

--- nthPrime.js
+++ nthPrime.js
@@ -1,14 +1,17 @@
+//Problem: Find Nth prime number
+//Example: nthPrime(5) => 11
+
 function nthPrime(n) {
   let count = 0; //how many primes we have
   let curNum = 1; //which num were looking at
   while (count < n) {
     curNum++;
-    console.log("count", count, "currentNum: ", curNum, checkPrime(curNum));
     if (checkPrime(curNum) === true) count++;
   }
   return curNum;
 }
 
+//helper function
 function checkPrime(num) {
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
@@ -17,5 +20,3 @@ function checkPrime(num) {
   }
   return true;
 }
-
-console.log(nthPrime(5));
