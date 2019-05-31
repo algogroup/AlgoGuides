a4dcbdd0d814bad9e69220b93a20f4646b2b3862
Daniel L <daniellanoff@gmail.com>
2019-05-31 16:58:20 -0400

    added wordSearch

---
+++ wordSearch.js
@@ -0,0 +1,64 @@
+// Given a 2D board and a word, find if the word exists in the grid.
+
+// The word can be constructed from letters of sequentially adjacent cell,
+//  where "adjacent" cells are those horizontally or vertically neighboring.
+//  The same letter cell may not be used more than once.
+
+//Given word = "SEED", return true.
+//Given word = "ABCB", return false.
+
+// let board = [["A", "B", "C", "E"],
+//              ["S", "F", "C", "S"],
+//              ["A", "D", "E", "E"]];
+let board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]];
+
+function findWord(board, search) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === search[0]) {
+        //found our starting point
+        let newCoord = checkNeighbors(board, [i, j], 0, search); // true or false [[2,3]]  OR [[2,3], [3,4]]
+        if (newCoord) return true;
+      }
+    }
+  }
+  return false;
+}
+
+function checkNeighbors(board, posArr, count, word) {
+  if (count === word.length - 1) return true;
+  board[posArr[0]][posArr[1]] = "+";
+  console.log(board);
+  // CHECK NEIGHBORS
+  if (
+    board[posArr[0] - 1] &&
+    board[posArr[0] - 1][posArr[1]] === word[count + 1]
+  )
+    if (checkNeighbors(board, [posArr[0] - 1, posArr[1]], count + 1, word))
+      return true;
+
+  if (
+    board[posArr[0] + 1] &&
+    board[posArr[0] + 1][posArr[1]] === word[count + 1]
+  )
+    if (checkNeighbors(board, [posArr[0] + 1, posArr[1]], count + 1, word))
+      return true;
+
+  if (
+    board[posArr[0]] &&
+    // posArr[1] - 1 &&
+    board[posArr[0]][posArr[1] - 1] === word[count + 1]
+  )
+    if (checkNeighbors(board, [posArr[0], posArr[1] - 1], count + 1, word))
+      return true;
+  if (
+    board[posArr[0]] &&
+    // posArr[1] + 1 &&
+    board[posArr[0]][posArr[1] + 1] === word[count + 1]
+  )
+    if (checkNeighbors(board, [posArr[0], posArr[1] + 1], count + 1, word))
+      //
+      return true;
+  board[posArr[0]][posArr[1]] = word[count];
+  return false;
+}
